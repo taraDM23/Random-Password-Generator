aac2aa659cffc0ffd0683a0a2916887997795c9b
tdm <55875354+taraDM23@users.noreply.github.com>
2019-10-25 16:01:05 +1100

    finalised layout, minor html grid modifications, js promt and alert comments.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var CharacterArray = [
 // slider choice
 var slider = document.getElementById("slider");
 var output = document.getElementById("ShowNum");
+
 output.innerHTML = slider.value;
 
 slider.oninput = function () {
@@ -38,27 +39,29 @@ function generate() {
     let values = "";
     let password = "";
 
-    if (document.getElementById("Numeric").checked) {
+    //Select an option alert
+   // if (document.querySelectorAll("#checkbox").checked = false){  
+    //alert("Please select at least one option");
+    //console.log("Test2");
+    //}
+
+    if (document.getElementById("Numeric").checked === true) {
         values += CharacterArray[0];
         console.log(values);
     }
-    if (document.getElementById("UpperCase").checked) {
-        values += CharacterArray[2];
-        console.log(values);
-    }
-    if (document.getElementById("LowerCase").checked) {
+    if (document.getElementById("LowerCase").checked === true) {
         values += CharacterArray[1];
         console.log(values);
     }
-    if (document.getElementById("Special").checked) {
+    if (document.getElementById("UpperCase").checked === true) {
+        values += CharacterArray[2];
+        console.log(values);
+    }
+    if (document.getElementById("Special").checked === true) {
         values += CharacterArray[3];
         console.log(values);
     }
-    //Select an option alert
-    else {
-        alert("Please select at least one option");
-   }
-
+   
     // slider calculation
     var slider = document.getElementById("slider");
     var output = document.getElementById("ShowNum");
@@ -67,13 +70,37 @@ function generate() {
     slider.oninput = function () {
         output.innerHTML = this.value;
     }
+    
+/* // You chose xyz please confirm options
 
-    // password
+//output.addEventListener("click", Reset);
+//console.log("clickcheck");
+
+//function Reset() {
+//console.log("confirmation");   
+//r = confirm("Continue!" + "show choices");
+
+
+    if (r == true) {
+        for (var i = 0; i < strength; i++) {
+            password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+        }
+        document.getElementById("output").value = password;  
+    }  
+    
+    else {location.reload()}
+}
+} */
+
+// password
     for (var i = 0; i < strength; i++) {
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
-    document.getElementById("output").value = password;   
+    document.getElementById("output").value = password;  
+    
 }
+
+
 
 
 
@@ -85,8 +112,6 @@ can choose:
 1. length  min 8 max 128
 
 js -counter - set from 8-128
-alart - if empty please select length between 8-128?
-
 
 2.Character types
 Special characters ([see examples](https://www.owasp.org/index.php/Password_special_characters))
@@ -100,12 +125,14 @@ max all 4 types
 alart - please select at least one charachter type
 
 Step 2 - generate password button
--show password on page
+
+new - Confirm selection yes no - 
+yes-show password on page
+no-refresh
 
 Step 3 - copy to clipboard button
 js- copy 
 
 
 Tri notes:
-Add the alart at the start of the code
- */
+Add the alart at the start of the code*/
